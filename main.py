7b91ae1b5fb79ec4884aa6960f75c674c4f0f9e7
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-05-11 22:08:26 +0300

    Adjustment to Multiple Line Texts + Improvements for Detection -- 3rd Commit

--- main.py
+++ main.py
@@ -6,131 +6,166 @@
 import os
 from dotenv import load_dotenv
 
-# Încarcă variabilele de mediu din fișierul .env
+# Încărcarea variabilelor de mediu din fișierul .env
 load_dotenv()
 
-# Accesează cheia API din variabilele de mediu
+# Accesarea cheii API din variabilele de mediu
 api_key = os.getenv('WORDSAPI_KEY')
 
-
+# Funcție pentru citirea textului dintr-un fișier
 def citeste_text_din_fisier(nume_fisier):
-    # Aceasta functie va deschide fisierul si va citi continutul acestuia.
     with open(nume_fisier, 'r', encoding='utf-8') as fisier:
-        continut = fisier.read()
+        continut = fisier.readlines()
     return continut
 
-cuvinte_vulgare = ['shit', 'fuck', 'fucking', 'bitch', 'dick', 'kys']  # Completează lista cu cuvintele vulgare reale.
-explicit = 0
+# Lista cuvintelor vulgare
+cuvinte_vulgare = ['shit', 'fuck', 'fucking', 'bitch', 'dick', 'kys', 'cum']
 
+
+
+# Funcție pentru cenzurarea cuvântului în funcție de lungime
 def cenzureaza_cuvant(cuvant):
-    # Cenzuram cuvântul în funcție de lungimea acestuia.
 
     print("\n" + cuvant)
 
-    cuvant_ajustat = cuvant.strip('", ;.!?-')
-
+    cuvant_ajustat = cuvant.strip('''"', ;.!?-''')
     lungime = len(cuvant_ajustat)
 
+    # Se va adăuga o cenzură pentru primele 4 litere, și încă una pentru fiecare 3 litere adiționale.
     numar_cenzuri = 1
     if lungime>4:
-        numar_cenzuri += (lungime-4)//3  # Calculam numărul de litere ce trebuie cenzurate.
+        numar_cenzuri += (lungime-4)//3
 
-    litere_cuvant_cenzurat = list(cuvant_ajustat)  # Transformam cuvantul într-o listă de caractere pentru a putea modifica literele.
+    litere_cuvant_cenzurat = list(cuvant_ajustat)
 
+    # De la a doua literă încolo, se vor insera cenzurile.
     for i in range(numar_cenzuri):
-        litere_cuvant_cenzurat[i+1] = '*'  # Cenzuram litera respectivă.
+        litere_cuvant_cenzurat[i+1] = '*'
 
-    cuvant_cenzurat = ''.join(litere_cuvant_cenzurat)  # Convertim lista înapoi într-un string.
+    cuvant_cenzurat = ''.join(litere_cuvant_cenzurat)
 
+    # Se va modifica secvența inițială, pentru a păstra delimitatori, semne de punctuație și alte simboluri.
     cuvant = cuvant.replace(cuvant_ajustat,cuvant_cenzurat)
 
     print( " { " + cuvant_ajustat + " | "+ cuvant_cenzurat + " | " + str(lungime) +  " | " + cuvant + " }")
-
     return cuvant
 
 
+# Funcție de verificare a cuvântului în dicționarul limbii engleze
 def verifica_cuvant(cuvant, api_key):
     import http.client
     import json
 
-    # Conectează-te la endpoint-ul WordsAPI
+    # Conectare la API-ul WordsAPI
     conn = http.client.HTTPSConnection("wordsapiv1.p.rapidapi.com")
 
-    # Pregătește header-urile pentru request
     headers = {
         'x-rapidapi-key': api_key,
         'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"
     }
 
-    # Formatează URL-ul pentru a include cuvântul căutat
+    # Construirea URL-ului pentru request
     url = f"/words/{cuvant}/frequency"
 
-    # Trimite requestul GET
+    # Realizarea request-ului
     conn.request("GET", url, headers=headers)
 
-    # Primește răspunsul
+    # Primirea răspunsul
     res = conn.getresponse()
     data = res.read()
 
-    # Decodifică și interpretează răspunsul
+    # Decodificarea răspunsului
     data_decoded = data.decode("utf-8")
     response = json.loads(data_decoded)
+    print(response)
 
-    # Verifică dacă cuvântul există în răspuns
+    # Verificarea existenței cuvântului în dicționar
     if 'word' in response:
         print(f"Cuvântul '{cuvant}' există în dicționarul limbii engleze.")
-        if response['frequency']['perMillion'] < 1:
-            print(f"Cuvântul '{cuvant}' este rar întâlnit.")
-            return "rar"
+
+        # Verificarea frecvenței de apariție a cuvântului
+        if 'frequency' in response:
+            if response['frequency']['perMillion'] < 1:
+                print(f"Cuvântul '{cuvant}' este rar întâlnit.")
+                return "rar"
+
         return True
+
     else:
         print(f"Cuvântul '{cuvant}' nu există în dicționarul limbii engleze.")
         return False
 
+# Funcție pentru testarea similitudinii cuvintelor
 def test_similitudine(cuvant):
+
     count_litere_similare = 0
     litere_cuvant = list(cuvant)
+
+    # Parsarea cuvintelor vulgare
     for cuvant_vulgar in cuvinte_vulgare:
+
         litere_cuvant_vulgar = list(cuvant_vulgar)
-        for litera in litere_cuvant:
-            if litera in litere_cuvant_vulgar:
-                count_litere_similare += 1
+        if len(litere_cuvant) == len(litere_cuvant_vulgar):
+            for i in range(len(litere_cuvant)):
+                if litere_cuvant[i] == litere_cuvant_vulgar[i]:
+                    count_litere_similare += 1
+
         if count_litere_similare + 1 == len(litere_cuvant_vulgar):
             return (1, cuvant_vulgar)
+
         count_litere_similare = 0
+
     return False
 
+# Funcție pentru procesarea textului
 def proceseaza_text(text, cuvinte_vulgare,explicit):
-    # Separăm textul în cuvinte și procesăm fiecare cuvânt.
+
     cuvinte = text.split()
     cuvinte_procesate = []
     explicit_count = 0
 
     for cuvant in cuvinte:
-        cuvant_original = cuvant.lower().strip('.,!?-"')  # Normalizăm cuvantul pentru comparație.
+        cuvant_original = cuvant.lower().strip('''"', ;.!?-''')
+        print(cuvant + " | " + cuvant_original)
         if cuvant_original in cuvinte_vulgare:
-            cuvant = cenzureaza_cuvant(cuvant)  # Aplicăm cenzura dacă cuvantul este vulgar.
+            cuvant = cenzureaza_cuvant(cuvant)
             explicit_count += 1
+
         else:
+            # Verificarea similitudinii cuvintelor
             similar = test_similitudine(cuvant_original)
             if similar:
+                # Dacă cuvântul este similar cu unul din lista cuvintelor vulgare, se va verifica dacă aparține limbii engleze.
                 verificare = verifica_cuvant(cuvant_original, api_key)
+
+                # Dacă cuvântul nu există în dicționarul limbii engleze sau este rar întâlnit, se va cenzura.
                 if verificare == False or verificare == "rar":
                     cuvant = cenzureaza_cuvant(cuvant_original)
                     explicit_count += 1
+
         cuvinte_procesate.append(cuvant)
 
     if explicit_count>=3:
         print("\n\nTextul contine " + str(explicit_count) + " cuvinte vulgare.")
         explicit = 1
 
-    return (' '.join(cuvinte_procesate),explicit)  # Reunim cuvintele înapoi într-un text.
+    text_procesat = ' '.join(cuvinte_procesate)
+    text_procesat += '\n'
+    return ( text_procesat,explicit)  # Reunim cuvintele înapoi într-un text.
 
 if __name__ == '__main__':
     text_de_intrare = citeste_text_din_fisier('input.txt')
-    text_sanitizat = proceseaza_text(text_de_intrare, cuvinte_vulgare, explicit)
-    if text_sanitizat[1]==1:
+    text_sanitizat = []
+    explicit = 0
+
+    for linie in text_de_intrare:
+        linie_procesata = proceseaza_text(linie, cuvinte_vulgare, explicit)
+        text_sanitizat.append(linie_procesata[0])
+        if linie_procesata[1]==1:
+            explicit = 1
+
+    if explicit==1:
         print("\n\nTextul a fost marcat ca fiind vulgar. Apar mai mult de 3 cuvinte vulgare.")
-    print(text_sanitizat[0])
+
     with open('output.txt', 'w', encoding='utf-8') as fisier:
-        fisier.write(text_sanitizat[0])
+        fisier.writelines(text_sanitizat)
